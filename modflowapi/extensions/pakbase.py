27b5c96b1457caeb115755c3958fe17754def61d
jlarsen <jlarsen@usgs.gov>
2024-07-29 11:45:59 -0700

    linting

--- modflowapi/extensions/pakbase.py
+++ modflowapi/extensions/pakbase.py
@@ -122,11 +122,7 @@ pkgvars = {
         "auxvar",
     ],
     # prt model
-    "mip": [
-        "porosity",
-        "retfactor",
-        "izone"
-    ],
+    "mip": ["porosity", "retfactor", "izone"],
     # exchange model
     "gwf-gwf": ["nexg", "nodem1", "nodem2", "cl1", "cl2", "ihc"],
     "gwt-gwt": ["nexg", "nodem1", "nodem2", "cl1", "cl2", "ihc"],
